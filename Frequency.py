6c5d38b5e7c62049d20a563ca4e08c056013edf3
TimWeberRE100 <Timothy.Weber@anu.edu.au>
2023-11-03 11:39:25 +1100

    Fix for negative storage

--- Frequency.py
+++ Frequency.py
@@ -5,15 +5,15 @@ import pylab as plt
 # Import matplotlib
 
 #demand_range = range(5,30,5)
-demand_range = range(5,10,5)
+demand_range = range(5,30,5)
 sample_spacing = 1 # [1/Hour]
 
 cutoff_frequencies = {
-                    "intraday": [0.5,1/23],
-                    "overnight": [1/23,1/25],
-                    "monthly": [1/25,1/(24*30)],
+                    "intraday": [0.5,1/20],
+                    "overnight": [1/20,1/28],
+                    "monthly": [1/28,1/(24*30)],
                     "seasonal": [1/(24*30),1/(24*30*12)],
-                    "longterm": [1/(24*30*12),1/(24*30*12*10)]
+                    "longterm": [1/(24*30*12),-1/(24*30*12*10)]
                     }
 
 taxonomy = list(cutoff_frequencies.keys())
@@ -22,19 +22,14 @@ for demand in demand_range:
     lpgm_df = pd.read_csv(f"Results/LPGM_SEAsiaMY{demand}.csv")
 
     time = np.linspace(0,8760*10,8760*10)
-    storage_timeseries_np = lpgm_df["Pumped hydro energy storage"].to_numpy()
+    storage_timeseries_np = lpgm_df["PHES-Storage"].to_numpy()
 
     W = rfftfreq(storage_timeseries_np.size,d=time[1] - time[0])
     storage_frequency = rfft(storage_timeseries_np)
+    print(storage_frequency)
     
-    category_storage_frequencies = {}
-    category_storage_timeseries = {}
-    first = True
     category_storage_frequencies_df = pd.DataFrame()
     category_storage_timeseries_df = pd.DataFrame()
-
-    """ category_storage_frequencies_df["Original"] = storage_frequency
-    category_storage_timeseries_df["Original"] = storage_timeseries_np """
 
     for category in taxonomy:
         cut_storage_frequency = storage_frequency.copy()
@@ -44,27 +39,53 @@ for demand in demand_range:
 
         cut_storage_frequency[W>cutoff_high] = 0
         cut_storage_frequency[W<=cutoff_low] = 0
-        cut_storage_frequency[-1] = 0
-
-        category_storage_frequencies[category] = cut_storage_frequency
-        category_storage_timeseries[category] = irfft(cut_storage_frequency)
 
         category_storage_frequencies_df[category] = cut_storage_frequency
-        category_storage_timeseries_df[category] = category_storage_timeseries[category]
+        category_storage_timeseries_df[category] = irfft(cut_storage_frequency)
 
-        """ if first:
-            category_storage_frequencies_df = pd.DataFrame(cut_storage_frequency)
-            category_storage_timeseries_df = pd.DataFrame(category_storage_timeseries[category])
-            first=False
-        else:
-            new_f_df = pd.DataFrame(cut_storage_frequency)
-            new_t_df = pd.DataFrame(category_storage_timeseries[category])
-            category_storage_frequencies_df = pd.concat([category_storage_frequencies_df,new_f_df])
-            category_storage_timeseries_df = pd.concat([category_storage_timeseries_df,new_t_df]) """
-
+    minimums = {}
+    maximums = {}
+    for category in taxonomy:
+        print(category)
+        minimums[category] = np.abs(min(np.array(category_storage_timeseries_df[category])))
+        for i in range(0,len(time)):        
+            if category != "longterm":                
+                category_storage_timeseries_df.at[i,category]=category_storage_timeseries_df.at[i,category] + minimums[category]
+            else:
+                category_storage_timeseries_df.at[i,category]=category_storage_timeseries_df.at[i,category] - minimums["intraday"] - minimums["overnight"] - minimums["monthly"] - minimums["seasonal"]
+        maximums[category] = max(np.array(category_storage_timeseries_df[category]))
     
-    print(category_storage_frequencies_df)
-    print(category_storage_timeseries_df)
+    minimums["longterm"] = np.abs(min(np.array(category_storage_timeseries_df["longterm"])))
+    for i in range(0,len(time)):
+        if category_storage_timeseries_df.at[i,"longterm"] < 0:
+            apportioning = abs(category_storage_timeseries_df.at[i,"longterm"])
+            apportioning_original = apportioning
+
+            original = category_storage_timeseries_df.at[i,"seasonal"]
+            category_storage_timeseries_df.at[i,"seasonal"] = max(0,category_storage_timeseries_df.at[i,"seasonal"]-apportioning)
+            apportioning += (category_storage_timeseries_df.at[i,"seasonal"] - original)
+
+            original = category_storage_timeseries_df.at[i,"monthly"]
+            category_storage_timeseries_df.at[i,"monthly"] = max(0,category_storage_timeseries_df.at[i,"monthly"]-apportioning)
+            apportioning += (category_storage_timeseries_df.at[i,"monthly"] - original)
+
+            original = category_storage_timeseries_df.at[i,"overnight"]
+            category_storage_timeseries_df.at[i,"overnight"] = max(0,category_storage_timeseries_df.at[i,"overnight"]-apportioning)
+            apportioning += (category_storage_timeseries_df.at[i,"overnight"] - original)
+
+            original = category_storage_timeseries_df.at[i,"intraday"]
+            category_storage_timeseries_df.at[i,"intraday"] = max(0,category_storage_timeseries_df.at[i,"intraday"]-apportioning)
+            apportioning += (category_storage_timeseries_df.at[i,"intraday"] - original)
+            
+
+            if apportioning > 0.01:
+                print(i,apportioning,apportioning_original)
+
+            category_storage_timeseries_df.at[i,"longterm"] = 0
+
+    category_storage_frequencies_df["Original"] = storage_frequency
+    category_storage_timeseries_df["Original"] = storage_timeseries_np
+    category_storage_frequencies_df["Frequencies"] = W
 
     category_storage_frequencies_df.to_csv(f"frequencies_{demand}.csv")
     category_storage_timeseries_df.to_csv(f"energy_profile_{demand}.csv")
---
+++ Preprocessing/test.py
@@ -0,0 +1,29 @@
+from scipy.fft import rfft, irfft, rfftfreq
+import pandas as pd
+import numpy as np
+import pylab as plt
+
+time = np.linspace(1,8760,8760)
+
+timeseries_df = pd.read_csv("/home/tim/Documents/Projects/Frequency Analysis/TestFunctions.csv")
+
+timeseries_names = list(timeseries_df.columns.values)
+
+category_storage_frequencies_df = pd.DataFrame()
+category_storage_timeseries_df = pd.DataFrame()
+
+for category in timeseries_names:
+    category_storage_frequencies_df[category] = rfft(np.array(timeseries_df[category]))
+    category_storage_timeseries_df[category] = irfft(np.array(category_storage_frequencies_df[category]))
+    W = rfftfreq(np.array(timeseries_df[category]).size,d=time[1] - time[0])
+
+    plt.subplot(2,2,1)
+    plt.plot(time,timeseries_df[category])
+    plt.xlim(0,8760)
+    plt.subplot(2,2,2)
+    plt.plot(W,category_storage_frequencies_df[category])
+    plt.xlim(0,1/100)
+    plt.subplot(2,2,3)
+    plt.plot(time,timeseries_df[category])
+    plt.xlim(0,8760)
+    plt.show() 
