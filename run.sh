4582e1525ac03abb2abbc1bda76a236750684587
TimWeberRE100 <timothy.weber@anu.edu.au>
2023-11-06 21:09:31 +1100

    Additional demand scenarios

--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-python3 Optimisation.py -e 10
-python3 Optimisation.py -e 15
-python3 Optimisation.py -e 20
-python3 Optimisation.py -e 25
+python3 Optimisation.py -e 6
+python3 Optimisation.py -e 7
+python3 Optimisation.py -e 8
+python3 Optimisation.py -e 9
+python3 Optimisation.py -e 11
+python3 Optimisation.py -e 12
+python3 Optimisation.py -e 13
+python3 Optimisation.py -e 14
+python3 Optimisation.py -e 16
+python3 Optimisation.py -e 17
+python3 Optimisation.py -e 18
+python3 Optimisation.py -e 19
+python3 Optimisation.py -e 21
+python3 Optimisation.py -e 22
+python3 Optimisation.py -e 23
+python3 Optimisation.py -e 24
